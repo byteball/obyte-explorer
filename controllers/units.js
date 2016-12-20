9c297c00fd6796cf369820dc8f3c1cbd628648bc
xjenek <clyde612@yandex.ru>
2016-12-20 22:09:09 +0300

    init

---
+++ controllers/units.js
@@ -0,0 +1,210 @@
+/*jslint node: true */
+'use strict';
+
+var db = require('byteballcore/db.js');
+var storage = require('byteballcore/storage.js');
+
+function getLastUnits(cb) {
+	var nodes = [];
+	var edges = {};
+
+	db.query("SELECT parenthoods.child_unit, parenthoods.parent_unit, units.ROWID, units.is_on_main_chain, units.is_stable, units.best_parent_unit \n\
+		FROM parenthoods, units WHERE parenthoods.child_unit IN \n\
+		(SELECT unit FROM units ORDER BY ROWID DESC LIMIT 0, 100) and units.unit=parenthoods.child_unit ORDER BY parenthoods.ROWID DESC", function(rows) {
+		rows.forEach(function(row) {
+			nodes.push({
+				data: {unit: row.child_unit, unit_s: row.child_unit.substr(0, 7) + '...'},
+				rowid: row.rowid,
+				is_on_main_chain: row.is_on_main_chain,
+				is_stable: row.is_stable
+			});
+			edges[row.child_unit + '_' + row.parent_unit] = {
+				data: {
+					source: row.child_unit,
+					target: row.parent_unit
+				},
+				best_parent_unit: row.parent_unit == row.best_parent_unit
+			};
+		});
+		cb(nodes, edges);
+	});
+}
+
+function getUnitsBeforeRowid(rowid, limit, cb) {
+	var nodes = [];
+	var edges = {};
+
+	db.query("SELECT parenthoods.child_unit, parenthoods.parent_unit, units.ROWID, units.is_on_main_chain, units.is_stable, units.best_parent_unit \n\
+		FROM parenthoods, units WHERE parenthoods.child_unit IN \n\
+		(SELECT unit FROM units WHERE ROWID < ? ORDER BY ROWID DESC LIMIT 0, ?) and units.unit=parenthoods.child_unit ORDER BY parenthoods.ROWID DESC", [rowid, limit], function(rows) {
+		rows.forEach(function(row) {
+			nodes.push({
+				data: {unit: row.child_unit, unit_s: row.child_unit.substr(0, 7) + '...'},
+				rowid: row.rowid,
+				is_on_main_chain: row.is_on_main_chain,
+				is_stable: row.is_stable
+			});
+			edges[row.child_unit + '_' + row.parent_unit] = {
+				data: {
+					source: row.child_unit,
+					target: row.parent_unit
+				},
+				best_parent_unit: row.parent_unit == row.best_parent_unit
+			};
+		});
+		cb(nodes, edges);
+	});
+}
+
+function getUnitsAfterRowid(rowid, limit, cb) {
+	var nodes = [];
+	var edges = {};
+	limit = limit ? 'LIMIT 0, ' + parseInt(limit) : '';
+
+	db.query("SELECT parenthoods.child_unit, parenthoods.parent_unit, units.ROWID, units.is_on_main_chain, units.is_stable, units.best_parent_unit \n\
+		FROM parenthoods, units WHERE parenthoods.child_unit IN \n\
+		(SELECT unit FROM units WHERE ROWID > ? ORDER BY ROWID ASC " + limit + ") and units.unit=parenthoods.child_unit ORDER BY parenthoods.ROWID DESC", [rowid], function(rows) {
+		rows.forEach(function(row) {
+			nodes.push({
+				data: {unit: row.child_unit, unit_s: row.child_unit.substr(0, 7) + '...'},
+				rowid: row.rowid,
+				is_on_main_chain: row.is_on_main_chain,
+				is_stable: row.is_stable
+			});
+			edges[row.child_unit + '_' + row.parent_unit] = {
+				data: {
+					source: row.child_unit,
+					target: row.parent_unit
+				},
+				best_parent_unit: row.parent_unit == row.best_parent_unit
+			};
+		});
+		cb(nodes, edges);
+	});
+}
+
+function getParentsAndChildren(unit, cb) {
+	var parents = [];
+	var children = [];
+	db.query(
+		"SELECT child_unit, parent_unit \n\
+		FROM parenthoods \n\
+		WHERE child_unit = ? or parent_unit = ? \n\
+		ORDER BY parent_unit",
+		[unit, unit],
+		function(rows) {
+			rows.forEach(function(row) {
+				if (row.child_unit === unit) {
+					parents.push(row.parent_unit);
+				}
+				else {
+					children.push(row.child_unit);
+				}
+			});
+			cb({parents: parents, children: children});
+		}
+	);
+}
+
+function getTransfersInfo(unit, cb) {
+	var transfersInfo = {};
+	db.query("SELECT outputs.output_index, outputs.unit, outputs.amount, outputs.asset FROM inputs, outputs \n\
+		WHERE inputs.unit = ? AND inputs.type = 'transfer' AND outputs.output_index = inputs.src_output_index \n\
+		AND outputs.unit = inputs.src_unit AND outputs.message_index = inputs.src_message_index", [unit], function(rows) {
+		rows.forEach(function(row) {
+			transfersInfo[row.unit + '_' + row.output_index + '_' + row.asset] = {unit: row.unit, amount: row.amount};
+		});
+		cb(transfersInfo);
+	});
+}
+
+
+function getSpentOutputs(unit, cb) {
+	var spentOutputs = {};
+	db.query("SELECT outputs.output_id, inputs.unit FROM outputs, inputs \n\
+		WHERE outputs.unit = ? AND is_spent = 1 AND inputs.src_unit = outputs.unit AND inputs.src_message_index = outputs.message_index \n\
+		AND inputs.src_output_index = outputs.output_index", [unit], function(rows) {
+		rows.forEach(function(row) {
+			spentOutputs[row.output_id] = row.unit;
+		});
+		cb(spentOutputs);
+	});
+}
+
+function getUnitOutputs(unit, cb) {
+	var unitOutputs = {};
+	getSpentOutputs(unit, function(outputsSpent) {
+		db.query("SELECT output_id, address, amount, asset, denomination, is_spent FROM outputs WHERE unit = ? ORDER BY output_index", [unit], function(rows) {
+			rows.forEach(function(row) {
+				if (!unitOutputs[row.asset]) {
+					unitOutputs[row.asset] = [];
+					if (outputsSpent[row.output_id]) {
+						row.spent = outputsSpent[row.output_id];
+					}
+					else {
+						row.spent = false;
+					}
+				}
+				unitOutputs[row.asset].push(row);
+			});
+			cb(unitOutputs);
+		});
+	});
+}
+
+function getInfoOnUnit(unit, cb) {
+	storage.readUnitProps(db, unit, function(unitProps) {
+		storage.readJoint(db, unit, {
+			ifFound: function(objJoint) {
+				getParentsAndChildren(unit, function(objParentsAndChildren) {
+					getTransfersInfo(unit, function(transfersInfo) {
+						getUnitOutputs(unit, function(outputsUnit) {
+							var objInfo = {
+								unit: unit,
+								child: objParentsAndChildren.children,
+								parents: objParentsAndChildren.parents,
+								authors: objJoint.unit.authors,
+								headers_commission: objJoint.unit.headers_commission,
+								payload_commission: objJoint.unit.payload_commission,
+								main_chain_index: unitProps.main_chain_index,
+								latest_included_mc_index: unitProps.latest_included_mc_index,
+								level: unitProps.level,
+								is_stable: unitProps.is_stable,
+								messages: objJoint.unit.messages,
+								transfersInfo: transfersInfo,
+								outputsUnit: outputsUnit
+							};
+							if (objJoint.unit.witnesses) {
+								objInfo.witnesses = objJoint.unit.witnesses;
+								cb(objInfo);
+							}
+							else {
+								storage.readWitnesses(db, unit, function(arrWitnesses) {
+									objInfo.witnesses = arrWitnesses;
+									cb(objInfo);
+								});
+							}
+						});
+					});
+				});
+			},
+			ifNotFound: function() {
+				cb(null);
+			}
+		});
+	});
+}
+
+
+function getUnitsThatBecameStable(arrUnits, cb) {
+	if (!arrUnits.length) return cb([]);
+	db.query("SELECT unit, is_on_main_chain, is_stable FROM units WHERE unit IN (?) and is_stable = 1", [arrUnits], function(rows) {
+		cb(rows);
+	});
+}
+
+exports.getLastUnits = getLastUnits;
+exports.getUnitsBeforeRowid = getUnitsBeforeRowid;
+exports.getUnitsAfterRowid = getUnitsAfterRowid;
+exports.getInfoOnUnit = getInfoOnUnit;
+exports.getUnitsThatBecameStable = getUnitsThatBecameStable;
---
+++ controllers/ws.js
@@ -0,0 +1,168 @@
+/*jslint node: true */
+'use strict';
+
+var db = require('byteballcore/db.js');
+var units = require('./units');
+var address = require('./address');
+
+function start(data) {
+	var ws = this;
+
+	if (data.type === 'last') {
+		units.getLastUnits(function(nodes, edges) {
+			ws.emit('start', {
+				nodes: nodes,
+				edges: edges
+			});
+		});
+	}
+	else if (data.type === 'unit') {
+		db.query("SELECT ROWID FROM units WHERE unit = ? LIMIT 0,1", [data.unit], function(row) {
+			if (!row.length) {
+				units.getLastUnits(function(nodes, edges) {
+					ws.emit('start', {
+						nodes: nodes,
+						edges: edges,
+						not_found: true
+					});
+				});
+			}
+			else {
+				units.getUnitsBeforeRowid(row[0].rowid + 25, 100, function(nodes, edges) {
+					ws.emit('start', {
+						nodes: nodes,
+						edges: edges
+					});
+				});
+			}
+		});
+	}
+	else if (data.type === 'address') {
+		address.getAddressInfo(data.address, function(objTransactions, unspent, objBalance, end) {
+			if (objTransactions === null) {
+				ws.emit('addressInfo');
+			}
+			else {
+				ws.emit('addressInfo', {
+					address: data.address,
+					objTransactions: objTransactions,
+					unspent: unspent,
+					objBalance: objBalance,
+					end: end
+				});
+			}
+		});
+	}
+}
+
+function next(data) {
+	var ws = this;
+
+	units.getUnitsThatBecameStable(data.notStable, function(arrStableUnits) {
+		units.getUnitsBeforeRowid(data.last, 100, function(nodes, edges) {
+			ws.emit('next', {
+				nodes: nodes,
+				edges: edges,
+				arrStableUnits: arrStableUnits
+			});
+		});
+	});
+}
+
+function prev(data) {
+	var ws = this;
+
+	units.getUnitsThatBecameStable(data.notStable, function(arrStableUnits) {
+		units.getUnitsAfterRowid(data.first, 100, function(nodes, edges) {
+			ws.emit('prev', {
+				nodes: nodes,
+				edges: edges,
+				end: nodes.length < 100,
+				arrStableUnits: arrStableUnits
+			});
+		});
+	});
+}
+
+function newUnits(data) {
+	var ws = this;
+
+	units.getUnitsThatBecameStable(data.notStable, function(arrStableUnits) {
+		units.getUnitsAfterRowid(data.unit, false, function(nodes, edges) {
+			ws.emit('new', {
+				nodes: nodes,
+				edges: edges,
+				arrStableUnits: arrStableUnits
+			});
+		});
+	});
+}
+
+
+function info(data) {
+	var ws = this;
+
+	units.getInfoOnUnit(data.unit, function(objInfo) {
+		if (objInfo) {
+			ws.emit('info', objInfo);
+		}
+	});
+}
+
+function highlightNode(data) {
+	var ws = this;
+
+	db.query("SELECT ROWID FROM units WHERE unit = ? LIMIT 0,1", [data.unit], function(row) {
+		if (row.length) {
+			var rowid = row[0].rowid;
+			if (rowid > data.first && rowid < data.first + 200) {
+				units.getUnitsAfterRowid(data.first, 200, function(nodes, edges) {
+					ws.emit('prev', {
+						nodes: nodes,
+						edges: edges,
+						end: nodes.length < 100
+					});
+				});
+			}
+			else if (rowid < data.last && rowid > data.last - 200) {
+				units.getUnitsBeforeRowid(data.last, 200, function(nodes, edges) {
+					ws.emit('next', {
+						nodes: nodes,
+						edges: edges
+					});
+				});
+			}
+			else {
+				units.getUnitsBeforeRowid(rowid + 25, 100, function(nodes, edges) {
+					ws.emit('start', {
+						nodes: nodes,
+						edges: edges
+					});
+				});
+			}
+		}
+		else {
+			ws.emit('info');
+		}
+	});
+}
+
+function nextPageTransactions(data) {
+	var ws = this;
+
+	address.getAddressTransactions(data.address, data.page, function(objTransactions) {
+		ws.emit('nextPageTransactions', {
+			address: data.address,
+			objTransactions: objTransactions,
+			end: objTransactions === null || Object.keys(objTransactions).length < 5
+		});
+	});
+}
+
+exports.start = start;
+exports.next = next;
+exports.prev = prev;
+exports.newUnits = newUnits;
+exports.info = info;
+exports.highlightNode = highlightNode;
+exports.nextPageTransactions = nextPageTransactions;
