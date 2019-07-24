4ba5b34fa1b00f61e7db9fff30afef45fd5c288c
tony <tonyofbyteball@users.noreply.github.com>
2019-07-24 17:51:25 +0300

    get address definition even if no history

--- controllers/ws.js
+++ controllers/ws.js
@@ -43,8 +43,10 @@ function start(data) {
 		SELECT unit FROM inputs WHERE address = ? \n\
 		UNION \n\
 		SELECT unit FROM outputs WHERE address = ? LIMIT 0,1", [data.address, data.address, data.address], function(rows) {
-			if(rows.length) {
+			if(rows.length || 1) {
 				address.getAddressInfo(data.address, data.filter || {}, function(objTransactions, unspent, objBalance, end, definition, newLastInputsROWID, newLastOutputsROWID) {
+					if (!objTransactions && !definition)
+						return ws.emit('addressInfo');
 					ws.emit('addressInfo', {
 						address: data.address,
 						objTransactions: objTransactions,
